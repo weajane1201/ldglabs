28068c2bbfe82448da609647b89b780a96a22cbf
Wea Jane <weajane01@gmail.com>
2021-03-22 14:14:27 -0700

    New Changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,6 +3,7 @@
         <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 
